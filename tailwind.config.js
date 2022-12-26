55c4cc7bb5673abf466f31f12eca9c657ba8345b
Ali5hahriar <hemadiali@gmail.com>
2022-12-26 01:11:56 +0000

    Diba

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/index.html"],
+  theme: {
+    extend: {
+      // backgroundImage: {
+      //   'hero-pattern': "url('./src/img/grill-veg.jpg')"
+      // }
+    },
+  },
+  plugins: [],
+}
